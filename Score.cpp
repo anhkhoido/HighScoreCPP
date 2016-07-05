8facf22c6600e25cc99420df091f8d9aa40da1fc
Anh Khoi Do <anhkhoido@gmail.com>
2016-07-05 19:53:34 -0400

    Creation of Score.cpp

---
+++ Score.cpp
@@ -0,0 +1,64 @@
+//
+// File's name: Score.cpp 
+// Author: Anh Khoi Do
+// Date: 2016-07-04 17:30:00
+// Version: 1.0
+//
+// Description: This is the implementation file of a
+// class that represents the score of a player.
+//
+// REVISION HISTORY
+//
+// DATE           Author             REASON(S)
+// ----------------------------------------------------------
+// 2016-07-04     Anh Khoi Do        Creation of the file.
+//===========================================================
+
+#include "Score.h"
+
+
+Score* Score::_instance = 0;
+
+Score::Score() { _score = 0; }
+
+Score::Score(int s)
+{
+	_score = s;
+}
+
+
+Score* Score::Instance()
+{
+	if (_instance == 0) _instance = new Score;
+	else std::cout << "Unique reference to the score already exists." << std::endl;
+
+	return _instance;
+}
+
+
+void Score::setScore(int s)
+{
+	_score = s;
+}
+
+
+int Score::getScore() const 
+{
+	return _score;
+}
+
+void Score::destroy()
+{
+	if (_instance != 0) {
+		delete _instance;
+		_instance = 0;
+	} else std::cout << "The singleton for the score is already destroyed!" << std::endl;
+}
+
+
+std::ostream& Score::read(std::ostream& os) const
+{
+	os << "Score: " << _score << std::endl;
+
+	return os;
+}
